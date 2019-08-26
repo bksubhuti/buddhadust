3e0cbc53d2598828c7d54bc2adf08516f80dcdb2
Stephen Torrence <captainvalor@gmail.com>
2019-08-26 08:55:49 -0400

    Update Credits style

--- pts-ebooks/dn/SigilRegexDN/plugin.py
+++ pts-ebooks/dn/SigilRegexDN/plugin.py
@@ -25,26 +25,33 @@ def run(bk):
         # Remove Headers + Homepage Line + Nav
         html = re.sub(r'<head>.*Sections<\/a>]<\/p>',r'<head><title></title></head><body>', html, 0, re.DOTALL)
 
+
+        # REFORMAT TITLES
+
         # Reformat Titles
-        html = re.sub(r'<h4 class=\"ctr\">.*Sutta (\d\+\+).*>([^>]* Suttan?t?a?ṃ?).*<h1>(.*)<\/h1>',r'<h1>\1. \3</h1><h3>\2</h3>', html, 0, re.DOTALL)
+        #html = re.sub(r'<h4 class=\"ctr\">.*Sutta (\d\+\+).*>([^>]* Suttan?t?a?ṃ?).*<h1>(.*)<\/h1>',r'<h1>\1. \3</h1><h3>\2</h3>', html, 0, re.DOTALL)
 
         # Reformat Titles part b
-        html = re.sub(r'(33.*)(<h4[^>]*>)([\w\s]+)(<\/h4>)',r'\1<h2>\3</h2>', html, 0, re.DOTALL)
+        #html = re.sub(r'(33.*)(<h4[^>]*>)([\w\s]+)(<\/h4>)',r'\1<h2>\3</h2>', html, 0, re.DOTALL)
 
         # Reformat Introductions part c
-        html = re.sub(r'<h4[^\n]*Introduction.* Sutta?n?t?a?\.?.*<\/h4>',r'<h2>Introduction</h2>', html, 0, re.DOTALL)
+        #html = re.sub(r'<h4[^\n]*Introduction.* Sutta?n?t?a?\.?.*<\/h4>',r'<h2>Introduction</h2>', html, 0, re.DOTALL)
 
         # Strip roman numerals part d
-        html = re.sub(r'<h[34][^<]*>[XVI]+\. (.*Suttan?n?t?a?.*)<\/h[34]>',r'<h2>\1</h2>', html, 0, re.DOTALL)
+        #html = re.sub(r'<h[34][^<]*>[XVI]+\. (.*Suttan?n?t?a?.*)<\/h[34]>',r'<h2>\1</h2>', html, 0, re.DOTALL)
 
         # Strip roman numerals part d1
-        html = re.sub(r'<h3 [^>]*>(.*)<\/h3>',r'<h2>\1</h2>', html, 0, re.DOTALL)
+        #html = re.sub(r'<h3 [^>]*>(.*)<\/h3>',r'<h2>\1</h2>', html, 0, re.DOTALL)
 
         # Decrement Second Title's Header for TOC part e
-        html = re.sub(r'<h1>([^\.]*)<\/h1>',r'<h3>\1</h3>', html, 0, re.DOTALL)
+        #html = re.sub(r'<h1>([^\.]*)<\/h1>',r'<h3>\1</h3>', html, 0, re.DOTALL)
+
 
         # Remove Translation Links
         html = re.sub(r'<span class="f[34]">\[?<[ab].*\]<\/span> ',r'', html, 0, 0)
+
+
+        # REFORMAT NOTE LINKS
         
         # Rename Note Links
         html = re.sub(r'(<sup>.*<)(a)(.*\/)(a)>(?=.*<\/sup>)',r'\1aNOTE\3aNOTE>', html, 0, 0)
@@ -55,8 +62,9 @@ def run(bk):
         # Restore Note Links
         html = re.sub(r'aNOTE',r'a', html, 0, 0)
 
-        # Remove inline images
-        html = re.sub(r'<div class="float[lr](?:pp)?.*<\/div>',r'', html, 0, re.DOTALL)
+
+        # Remove Float Boxes / Inline Images
+        html = re.sub(r'<div class="float[lr](?:pp)?.*?<\/div>',r'', html, 0, re.DOTALL)
 
         # Remove Footers
         html = re.sub(r'(?:<p class="ctr"(?: style="margin-top: 4px")?>&#160;\[(?:Contents |Ones).*)?<\/div>\n\n<hr\/>\n?\n?<p class="fine ctr c">.*<\/p>',r'</div><hr/>', html, 0, re.DOTALL)
@@ -65,7 +73,7 @@ def run(bk):
         #html = re.sub(r'<p class="ctr">.*(?:Use|permission|Chow|Genaud).<\/p>',r'', html, 0, re.DOTALL) 
         
         # Remove Brackets on Endnotes
-        #html = re.sub(r'(<sup>.*?)\[(.*?)\](.*?<\/sup>)',r'\1\2\3', html, 0, 0)
+        html = re.sub(r'(<sup>.*?)\[(.*?)\](.*?<\/sup>)',r'\1\2\3', html, 0, 0)
 
         
  
